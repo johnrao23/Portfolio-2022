0b31782baa50ad6ee147cfd949ebe8e7593468d3
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-10-08 23:38:31 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -489,7 +489,7 @@ Ammo().then((Ammo) => {
       var xMid, text;
 
       // var color = 0xfffc00;
-      var color = 0x0000ff;
+      var color = 0xff6600;
 
       var textMaterials = [
         new THREE.MeshBasicMaterial({ color: color }), // front
