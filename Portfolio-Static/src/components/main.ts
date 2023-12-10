072995cc046d2abadd2326b705e2646f0df984c5
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-12-09 20:17:31 -0500

    Update main.ts

--- Portfolio-Static/src/components/main.ts
+++ Portfolio-Static/src/components/main.ts
@@ -1,9 +1,13 @@
 import React from "react"
 
 const Main = () => {
+
+    const Hello = 1;
     return (
         <div>
-          <h1>Hello</ h1>
+          <h1>{Hello}</ h1>
         </div>
     )
 }
+
+export default Main;
