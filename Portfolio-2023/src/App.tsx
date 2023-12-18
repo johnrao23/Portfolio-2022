9e454694d6840afdf1b02653349835eb52b04b40
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-12-18 15:49:57 -0500

    ..

--- Portfolio-2023/src/App.tsx
+++ Portfolio-2023/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import Main from './static/components/Main'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './static/components/Main';
 
 const App: React.FC = () => {
-
   return (
-    <div>
-      <Main />
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/static" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- Portfolio-2023/src/index.tsx
+++ Portfolio-2023/src/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Assuming App is the default export from App.tsx
+import Main from './3d/components/Main.js';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+const pathname = window.location.pathname;
+if (pathname.startsWith('/static')) {
+  // Render the React application
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+} else {
+  // Render the Three.js application
+  root.render(
+    <React.StrictMode>
+      <Main />
+    </React.StrictMode>,
+  );
+}
