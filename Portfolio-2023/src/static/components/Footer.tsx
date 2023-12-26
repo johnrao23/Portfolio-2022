8a54e564eab2581b3c96de637cf3592b543ca427
John Rao <86802699+johnrao23@users.noreply.github.com>
2023-12-26 15:07:10 -0500

    Update Footer.tsx

--- Portfolio-2023/src/static/components/Footer.tsx
+++ Portfolio-2023/src/static/components/Footer.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import { GridPattern } from '@/components/GridPattern'
 
-const Footer: React.FC = () => {
-    return (
-        <div>
-            <h1>Footer</h1>
-        </div>
-    )
+export function Footer() {
+  return (
+    <footer className="relative pb-20 pt-5 sm:pb-32 sm:pt-14">
+      <div className="absolute inset-x-0 top-0 h-32 text-slate-900/10 [mask-image:linear-gradient(white,transparent)]">
+        <GridPattern x="50%" />
+      </div>
+      <div className="relative text-center text-sm text-slate-600">
+        <p>Copyright &copy; {new Date().getFullYear()} Lindehoff Design, LLC</p>
+        <p>All rights reserved.</p>
+      </div>
+    </footer>
+  )
 }
-
-export default Footer;
